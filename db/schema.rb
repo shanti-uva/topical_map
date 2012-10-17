f6ef07e09cd67372337cd98213980759ef79f42a
Andres Montano <amontano@virginia.edu>
2012-10-17 03:39:53 -0600

    updating engines

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121011190717) do
+ActiveRecord::Schema.define(:version => 20121017093100) do
 
   create_table "authors_descriptions", :id => false, :force => true do |t|
     t.integer "author_id",      :null => false
